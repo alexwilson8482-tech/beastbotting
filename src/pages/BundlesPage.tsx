4d6e8904284ad111b29acc4221f951e3aceb8b96
alexwilson8482-tech <alexwilson8482@gmail.com>
2026-08-21 08:04:10 +0530

    Update BundlesPage.tsx

--- src/pages/BundlesPage.tsx
+++ src/pages/BundlesPage.tsx
@@ -1,45 +1,6 @@
 import { BundleManager } from "../components/BundleManager";
-import type { ApiPanel, Bundle } from "../types/order";
-
-interface BundlesPageProps {
-  apis: ApiPanel[];
-  bundles: Bundle[];
-  onAddBundle: (bundle: {
-    name: string;
-    apiId: string;
-    views: string;
-    likes: string;
-    shares: string;
-    saves: string;
-    comments: string;
-    reposts: string;
-  }) => void;
-  onUpdateBundle: (
-    id: string,
-    bundle: {
-      name: string;
-      apiId: string;
-      views: string;
-      likes: string;
-      shares: string;
-      saves: string;
-      comments: string;
-      reposts: string;
-    }
-  ) => void;
-  onDeleteBundle: (id: string) => void;
-}
-
-export function BundlesPage({ apis, bundles, onAddBundle, onUpdateBundle, onDeleteBundle }: BundlesPageProps) {
-  return (
-    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 sm:py-8">
-      <BundleManager
-        apis={apis}
-        bundles={bundles}
-        onAddBundle={onAddBundle}
-        onUpdateBundle={onUpdateBundle}
-        onDeleteBundle={onDeleteBundle}
-      />
-    </div>
-  );
+import type { ApiPanel, Bundle, BundleServiceSelections } from "../types/order";
+interface BundleInput { name:string; rotations:BundleServiceSelections }
+export function BundlesPage({ apis, bundles, onAddBundle, onUpdateBundle, onDeleteBundle }: { apis:ApiPanel[]; bundles:Bundle[]; onAddBundle:(bundle:BundleInput)=>void; onUpdateBundle:(id:string,bundle:BundleInput)=>void; onDeleteBundle:(id:string)=>void }) {
+  return <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 sm:py-8"><BundleManager apis={apis} bundles={bundles} onAddBundle={onAddBundle} onUpdateBundle={onUpdateBundle} onDeleteBundle={onDeleteBundle}/></div>;
 }
